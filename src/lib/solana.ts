c4b826e81f631e2198888b595237447d28779038
Abhyuday <abhyuday911@gmail.com>
2025-12-22 07:01:10 +0530

    feat: minor fixes.

--- src/lib/solana.ts
+++ src/lib/solana.ts
@@ -1,2 +1,2 @@
 import { Connection } from '@solana/web3.js';
-export const RpcConnection = new Connection("https://mainnet.helius-rpc.com/?api-key=f62a066e-9cc7-4d20-afa8-2ae157308e82", "confirmed")
+export const RpcConnection = new Connection(process.env.NEXT_PUBLIC_SOLANA_RPC_URL!, "confirmed")
